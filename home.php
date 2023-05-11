88102014bba6c120c85b84eca6cd19262dbd1752
Van <signingvan@yahoo.com>
2023-05-11 16:41:16 +0100

    Ajout des icones et amélioration du design

--- home.php
+++ home.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/pc/home.css">
+    <link rel="stylesheet" href="icons/css/all.css">
     <title>CHAT</title>
 </head>
 <body>
@@ -17,6 +18,7 @@
                 <div class="header-leftSide">
                     <div class="user-info">
                         <span class="photo-profil">
+                            <img src="pictures/iconDefault.png" alt="">
 
                         </span>
 
@@ -24,20 +26,113 @@
 
                     </div>
                     <span class="menu-general">
-
+                        <svg viewBox="0 0 24 24" width="24" height="24" class=""><path fill="currentColor" d="M12 7a2 2 0 1 0-.001-4.001A2 2 0 0 0 12 7zm0 2a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 9zm0 6a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 15z"></path></svg>
                     </span>
 
                 </div>
                 <div class="search-bar">
                     <form action="" class="form-search">
-                        <input type="text" name="" id="">
-                        <button type="reset">re</button>
-                        <button type="submit">se</button>
+                        <input type="text" name="" id="search" placeholder="Search...">
+                        <button type="reset" id="btnReset">
+                            <i class="fa-sharp fa-solid fa-xmark"></i>
+                        </button>
+                        <button type="submit">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </button>
                     </form>
 
                 </div>
                 <div class="members-resultSearch">
                     <div class="members">
+                        <div class="item-chat">
+                            <span class="item-photo">
+                                <img src="pictures/iconDefault.png" alt="user photo">
+                            </span>
+                            <div class="user">
+                                <p>Username</p>
+                            </div>
+                        </div>
+
+                        <div class="item-chat">
+                            <span class="item-photo">
+                                <img src="pictures/iconDefault.png" alt="user photo">
+                            </span>
+                            <div class="user">
+                                <p>Username</p>
+                            </div>
+                        </div>
+
+                        <div class="item-chat">
+                            <span class="item-photo">
+                                <img src="pictures/iconDefault.png" alt="user photo">
+                            </span>
+                            <div class="user">
+                                <p>Username</p>
+                            </div>
+                        </div>
+
+                        <div class="item-chat">
+                            <span class="item-photo">
+                                <img src="pictures/iconDefault.png" alt="user photo">
+                            </span>
+                            <div class="user">
+                                <p>Username</p>
+                            </div>
+                        </div>
+
+                        <div class="item-chat">
+                            <span class="item-photo">
+                                <img src="pictures/iconDefault.png" alt="user photo">
+                            </span>
+                            <div class="user">
+                                <p>Username</p>
+                            </div>
+                        </div>
+
+                        <div class="item-chat">
+                            <span class="item-photo">
+                                <img src="pictures/iconDefault.png" alt="user photo">
+                            </span>
+                            <div class="user">
+                                <p>Username</p>
+                            </div>
+                        </div>
+
+                        <div class="item-chat">
+                            <span class="item-photo">
+                                <img src="pictures/iconDefault.png" alt="user photo">
+                            </span>
+                            <div class="user">
+                                <p>Username</p>
+                            </div>
+                        </div>
+
+                        <div class="item-chat">
+                            <span class="item-photo">
+                                <img src="pictures/iconDefault.png" alt="user photo">
+                            </span>
+                            <div class="user">
+                                <p>Username</p>
+                            </div>
+                        </div>
+
+                        <div class="item-chat">
+                            <span class="item-photo">
+                                <img src="pictures/iconDefault.png" alt="user photo">
+                            </span>
+                            <div class="user">
+                                <p>Username</p>
+                            </div>
+                        </div>
+
+                        <div class="item-chat">
+                            <span class="item-photo">
+                                <img src="pictures/iconDefault.png" alt="user photo">
+                            </span>
+                            <div class="user">
+                                <p>Username</p>
+                            </div>
+                        </div>
 
                     </div>
 
@@ -51,30 +146,36 @@
                     <div class="header-chat">
                         <div class="info-destinataire">
                             <span class="photo-destinataire">
+                                <img src="pictures/iconDefault.png" alt="">
 
                             </span>
-                            <p>Destinataire</p>
+                            <p>Username</p>
 
                         </div>
                         <span class="menu-destinataire">
+                            <svg viewBox="0 0 24 24" width="24" height="24" class=""><path fill="currentColor" d="M12 7a2 2 0 1 0-.001-4.001A2 2 0 0 0 12 7zm0 2a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 9zm0 6a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 15z"></path></svg>
 
                         </span>
 
                     </div>
 
                     <div class="discussion">
+                        
 
                     </div>
                     
 
                     <form action="" method="POST" enctype="multipart/form-data" class="footer-chat">
 
-                        <div class="menu-file">
+                        <button class="menu-file">
+                            <i class="fa-solid fa-paperclip"></i>
+                            
+                        </button>
+                        <textarea name="messages" id="msg" class="message" placeholder="Send message..." required autofocus></textarea>
 
-                        </div>
-                        <textarea name="messages" id="msg" class="message" placeholder="send message" require focus></textarea>
-
-                        <button type="submit" class="send-message">Se</button>
+                        <button type="submit" class="send-message">
+                            <i class="fa-solid fa-paper-plane"></i>
+                        </button>
 
                     </form>
 
@@ -86,6 +187,7 @@
     </div>
 
     <script src="js/scrollTextarea.js"></script>
+    <script src="js/searchBar"></script>
     
 </body>
 </html>
